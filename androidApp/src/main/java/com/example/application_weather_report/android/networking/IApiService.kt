66270d064b77735055d7181d94e0b0bc0b6cf795
Tom <tom.charpentier@etu.unilim.fr>
2024-03-21 18:29:01 +0100

    ajout apiservice

---
+++ androidApp/src/main/java/com/example/application_weather_report/android/networking/IApiService.kt
@@ -0,0 +1,17 @@
+package com.example.application_weather_report.android.networking
+
+import com.example.application_weather_report.android.model.Response
+import retrofit2.Call
+import retrofit2.http.*
+
+
+interface IApiService {
+
+    // Get current weather data
+    @GET("current.json")
+    fun getCurrentWeather(
+        @Query("key") key: String = ApiConfig.API_KEY,
+        @Query("q") city: String,
+        @Query("aqi") aqi: String = "no"
+    ): Call<Response>
+}
