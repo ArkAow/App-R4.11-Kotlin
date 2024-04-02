40f13f30d4bfbc8a5dd1215862bcfe9ef9ba37b1
Tom <tom.charpentier@etu.unilim.fr>
2024-04-02 17:26:41 +0200

    ajout activité pour changer les favoris

---
+++ androidWeatherApp/src/main/java/fr/unilim/weatherapplication/android/FavoritesManagerActivity.kt
@@ -0,0 +1,12 @@
+package fr.unilim.weatherapplication.android
+
+import android.os.Bundle
+
+class FavoritesManagerActivity : AppCompatActivity() {
+    fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Mettez en place l'interface utilisateur pour la gestion des favoris
+        // Permettez à l'utilisateur d'ajouter ou de supprimer des villes de la liste des favoris
+        // Une fois terminé, renvoyez la liste mise à jour à MainActivity
+    }
+}
